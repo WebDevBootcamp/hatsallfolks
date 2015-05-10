433488fdb8414bd5832e25a0f20df45a4af42cb7
JW LeRoi <jack.leroi@ucdenver.edu>
2015-05-10 11:48:35 -0600

    2015.05.10.11:46
    Item Selection page with javascript, stylesheet, and bootstrap.

---
+++ scripts/js/functions.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Jack on 5/9/2015.
+ */
+$(document).ready(function()
+{
+    $("#pic01").on("click", function (event)
+    {
+        alert("Picture 01");
+    });
+    $("#pic02").on("click", function (event)
+    {
+        alert("Picture 02");
+    });
+    $("#pic03").on("click", function (event)
+    {
+        alert("Picture 03");
+    });
+    $("#pic04").on("click", function (event)
+    {
+        alert("Picture 04");
+    });
+    $("#pic05").on("click", function (event)
+    {
+        alert("Picture 05");
+    });
+});
