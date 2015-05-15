164c62dd4c31021382571f48e6b592dbb5518503
JW LeRoi <jack.leroi@ucdenver.edu>
2015-05-15 06:53:46 -0600

    2015.05.15.06:53

--- scripts/js/functions.js
+++ scripts/js/functions.js
@@ -3,24 +3,5 @@
  */
 $(document).ready(function()
 {
-    $("#pic01").on("click", function (event)
-    {
-        alert("Picture 01");
-    });
-    $("#pic02").on("click", function (event)
-    {
-        alert("Picture 02");
-    });
-    $("#pic03").on("click", function (event)
-    {
-        alert("Picture 03");
-    });
-    $("#pic04").on("click", function (event)
-    {
-        alert("Picture 04");
-    });
-    $("#pic05").on("click", function (event)
-    {
-        alert("Picture 05");
-    });
+
 });
