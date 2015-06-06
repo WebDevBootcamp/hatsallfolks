8e9763e032a865602a33dd0dc423752d6fb50158
JW LeRoi <jack.leroi@ucdenver.edu>
2015-06-06 10:44:23 -0600

    2015.06.06.10:29 - Added delete link to items in shopping cart.
    cart.js
    Added code to remove a selected item from the shopping cart.
    
    cart.ejs
    Added div for delete link for each item on shopping cart page. Refactored variable name.

--- scripts/js/cart.js
+++ scripts/js/cart.js
@@ -1,14 +1,55 @@
 /**
  * Created by Jack on 6/1/2015.
  */
-$(document).ready(function(){
+$(document).ready(function()
+{
+  displayItems();
 
-  var cartCookie = docCookies.getItem("hatCart");
-  //console.log(cartCookie);
-  var items = JSON.parse(cartCookie);
-  //alert("Ready")
-  //console.log(items);
-  var html = new EJS({url: "views/cart.ejs"}).render({items:items});
-  $("#cart_container").html(html);
+  $("#cart_container").on("click", ".delete_item", function(event)
+  {
+    var removeItem = $(this).data("item_index");
+    var cartCookie = docCookies.getItem("hatCart", "/");
+    var items = JSON.parse(cartCookie);
+
+    if(items.length === 1)
+    {
+      docCookies.removeItem("hatCart");
+      docCookies.removeItem("hatCart", "/");
+      $("#cart_container").html("");
+      window.close();
+    }
+    else
+    {
+      var newCart = "[";
+
+      items.forEach(function (element, index)
+      {
+        if (index !== removeItem)
+        {
+          newCart = newCart + JSON.stringify(element) + ",";
+
+        }
+
+      });
+
+      newCart = newCart.substring(0, newCart.length - 1) + "]";
+
+      docCookies.removeItem("hatCart");
+      docCookies.removeItem("hatCart", "/");
+      docCookies.setItem("hatCart", newCart, "/");
+
+      displayItems();
+
+    }
+
+  });
 
 });
+
+function displayItems()
+{
+  var cartCookie = docCookies.getItem("hatCart", "/");
+  var items = JSON.parse(cartCookie);
+  var html = new EJS({url: "views/cart.ejs"}).render({items: items});
+  $("#cart_container").html(html);
+}
